7cdeab0f83508b4ab1339c7f671de3a656911176
Rafael Marques <rmcsoftdev@Rafaels-MacBook-Pro.local>
2021-04-16 09:20:36 -0300

    testing commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,7 @@ const App = ({ Component, pageProps }) => {
     <>
      <Provider store={store}>
          <Component {...pageProps} />  
-     </Provider>
-     
+     </Provider> 
     </>
   );
 };
